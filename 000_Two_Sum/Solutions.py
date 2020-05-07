a24e695f1f9a46e6ce218834affa76f6ea0d9c64
YujiaYANG <yujiayang@macbook-pro-de-yujiayang.home>
2020-05-07 21:09:34 +0200

    del

--- 000_Two_Sum/Solutions.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums, target):
-        dict = {}
-        for i,value in enumerate(nums):
-            if target-value in dict:
-                return dict[target-value],i
-            else:
-                dict[value] = i 
